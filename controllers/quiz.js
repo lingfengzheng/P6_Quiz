a728c7b1b88923adec61c981033b2edfc887e0aa
Lingfeng Zheng <Lingfeng.zheng@alumnos.upm.es>
2018-06-28 14:01:53 +0200

    Practica8

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,103 +1,30 @@
 const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
 const {models} = require("../models");
-
-const paginate = require('../helpers/paginate').paginate;
 
 // Autoload the quiz with id equals to :quizId
 exports.load = (req, res, next, quizId) => {
 
-    models.quiz.findById(quizId, {
-        include: [
-            {model:models.tip,
-                include:[
-                    {model:models.user, as:'author'}]},
-            {model: models.user, as: 'author'}
-
-
-        ]
+    models.quiz.findById(quizId)
+    .then(quiz => {
+        if (quiz) {
+            req.quiz = quiz;
+            next();
+        } else {
+            throw new Error('There is no quiz with id=' + quizId);
+        }
     })
-        .then(quiz => {
-            if (quiz) {
-                req.quiz = quiz;
-                next();
-            } else {
-                throw new Error('There is no quiz with id=' + quizId);
-            }
-        })
-        .catch(error => next(error));
-};
-
-
-// MW that allows actions only if the user logged in is admin or is the author of the quiz.
-exports.adminOrAuthorRequired = (req, res, next) => {
-
-    const isAdmin  = !!req.session.user.isAdmin;
-    const isAuthor = req.quiz.authorId === req.session.user.id;
-
-    if (isAdmin || isAuthor) {
-        next();
-    } else {
-        console.log('Prohibited operation: The logged in user is not the author of the quiz, nor an administrator.');
-        res.send(403);
-    }
+    .catch(error => next(error));
 };
 
 
 // GET /quizzes
 exports.index = (req, res, next) => {
 
-    let countOptions = {
-        where: {}
-    };
-
-    let title = "Questions";
-
-    // Search:
-    const search = req.query.search || '';
-    if (search) {
-        const search_like = "%" + search.replace(/ +/g,"%") + "%";
-
-        countOptions.where.question = { [Op.like]: search_like };
-    }
-
-    // If there exists "req.user", then only the quizzes of that user are shown
-    if (req.user) {
-        countOptions.where.authorId = req.user.id;
-        title = "Questions of " + req.user.username;
-    }
-
-    models.quiz.count(countOptions)
-        .then(count => {
-
-            // Pagination:
-
-            const items_per_page = 10;
-
-            // The page to show is given in the query
-            const pageno = parseInt(req.query.pageno) || 1;
-
-            // Create a String with the HTMl used to render the pagination buttons.
-            // This String is added to a local variable of res, which is used into the application layout file.
-            res.locals.paginate_control = paginate(count, items_per_page, pageno, req.url);
-
-            const findOptions = {
-                countOptions,
-                offset: items_per_page * (pageno - 1),
-                limit: items_per_page,
-                include: [{model: models.user, as: 'author'}]
-            };
-
-            return models.quiz.findAll(findOptions);
-        })
-        .then(quizzes => {
-            res.render('quizzes/index.ejs', {
-                quizzes,
-                search,
-                title
-            });
-        })
-        .catch(error => next(error));
+    models.quiz.findAll()
+    .then(quizzes => {
+        res.render('quizzes/index.ejs', {quizzes});
+    })
+    .catch(error => next(error));
 };
 
 
@@ -114,7 +41,7 @@ exports.show = (req, res, next) => {
 exports.new = (req, res, next) => {
 
     const quiz = {
-        question: "",
+        question: "", 
         answer: ""
     };
 
@@ -126,29 +53,28 @@ exports.create = (req, res, next) => {
 
     const {question, answer} = req.body;
 
-    const authorId = req.session.user && req.session.user.id || 0;
-
     const quiz = models.quiz.build({
         question,
-        answer,
-        authorId
+        answer
     });
 
+    req.session.toBeResolved = "";
+
     // Saves only the fields question and answer into the DDBB
-    quiz.save({fields: ["question", "answer", "authorId"]})
-        .then(quiz => {
-            req.flash('success', 'Quiz created successfully.');
-            res.redirect('/quizzes/' + quiz.id);
-        })
-        .catch(Sequelize.ValidationError, error => {
-            req.flash('error', 'There are errors in the form:');
-            error.errors.forEach(({message}) => req.flash('error', message));
-            res.render('quizzes/new', {quiz});
-        })
-        .catch(error => {
-            req.flash('error', 'Error creating a new Quiz: ' + error.message);
-            next(error);
-        });
+    quiz.save({fields: ["question", "answer"]})
+    .then(quiz => {
+        req.flash('success', 'Quiz created successfully.');
+        res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(Sequelize.ValidationError, error => {
+        req.flash('error', 'There are errors in the form:');
+        error.errors.forEach(({message}) => req.flash('error', message));
+        res.render('quizzes/new', {quiz});
+    })
+    .catch(error => {
+        req.flash('error', 'Error creating a new Quiz: ' + error.message);
+        next(error);
+    });
 };
 
 
@@ -170,19 +96,19 @@ exports.update = (req, res, next) => {
     quiz.answer = body.answer;
 
     quiz.save({fields: ["question", "answer"]})
-        .then(quiz => {
-            req.flash('success', 'Quiz edited successfully.');
-            res.redirect('/quizzes/' + quiz.id);
-        })
-        .catch(Sequelize.ValidationError, error => {
-            req.flash('error', 'There are errors in the form:');
-            error.errors.forEach(({message}) => req.flash('error', message));
-            res.render('quizzes/edit', {quiz});
-        })
-        .catch(error => {
-            req.flash('error', 'Error editing the Quiz: ' + error.message);
-            next(error);
-        });
+    .then(quiz => {
+        req.flash('success', 'Quiz edited successfully.');
+        res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(Sequelize.ValidationError, error => {
+        req.flash('error', 'There are errors in the form:');
+        error.errors.forEach(({message}) => req.flash('error', message));
+        res.render('quizzes/edit', {quiz});
+    })
+    .catch(error => {
+        req.flash('error', 'Error editing the Quiz: ' + error.message);
+        next(error);
+    });
 };
 
 
@@ -190,14 +116,14 @@ exports.update = (req, res, next) => {
 exports.destroy = (req, res, next) => {
 
     req.quiz.destroy()
-        .then(() => {
-            req.flash('success', 'Quiz deleted successfully.');
-            res.redirect('/goback');
-        })
-        .catch(error => {
-            req.flash('error', 'Error deleting the Quiz: ' + error.message);
-            next(error);
-        });
+    .then(() => {
+        req.flash('success', 'Quiz deleted successfully.');
+        res.redirect('/quizzes');
+    })
+    .catch(error => {
+        req.flash('error', 'Error deleting the Quiz: ' + error.message);
+        next(error);
+    });
 };
 
 
@@ -230,59 +156,73 @@ exports.check = (req, res, next) => {
     });
 };
 
-
-
-exports.randomplay=(req,res,next)=>{
-    if(req.session.randomplay===undefined){
-        req.session.randomplay=[];
+// GET /quizzes/randomplay
+exports.randomplay = (req, res, next) => {
+    if(req.session.toBeResolved){
+        renderQuiz(req, res, next);
+    } else {
+        req.session.toBeResolved = [];
+        req.session.score = 0;
+        let i = 0;
+        models.quiz.findAll()
+            .each(quiz => {
+                req.session.toBeResolved[i] = quiz.id;
+                i++;
+            })
+            .then(() => {
+                renderQuiz(req, res, next);
+            })
     }
-    const whereOpt={"id":{[Sequelize.Op.notIn]:req.session.randomplay}};
-    return models.quiz.count({where:whereOpt})
-        .then(count=>{
-            if(!count){
-                let score=req.session.randomplay.length;
-                req.session.randomplay=[];
-                res.render('quizzes/random_nomore',{score:score});
-            }
-
-            let ran =Math.floor(Math.random()*count);
-            return models.quiz.findAll({where:whereOpt,offset:ran,limit:1})
-                .then(quizzes=>{
-                    return quizzes[0];
-                });
-
-        })
-        .then(quiz=>{
-            let score =req.session.randomplay.length;
-            res.render('quizzes/random_play',{quiz,score});
-        })
-
-        .catch(error=>{
-            req.flash('error','Error al ejecutar randomplay: '+ error.message);
-            next(error);
-        })
 
 }
 
-exports.randomcheck=(req,res,next)=>{
+// GET /quizzes/randomcheck/:quizId?answer=respuesta
+exports.randomcheck = (req, res, next) => {
+    const {quiz, query} = req;
 
-    const {query,quiz}=req;
-
-    const answer = query.answer|| "";
+    const answer = query.answer || "";
     const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
 
-
-    if(result) {
-        if (req.session.randomplay.indexOf(quiz.id === -1)) {
-            req.session.randomplay.push(quiz.id);
-        }
-        let score =req.session.randomplay.length;
-        res.render('quizzes/random_result',{score,answer:answer,result:result});
-    }else {
-        let score =req.session.randomplay.length;
-        req.session.randomplay=[] ;
-        res.render('quizzes/random_result',{score,answer:answer,result:result});
-
+    if(result){
+        req.session.toBeResolved.splice(req.session.num,1);
+        req.session.score ++;
     }
+    const score = req.session.score;
 
+    if(req.session.toBeResolved.length === 0){
+        req.session.score = 0;
+        req.session.toBeResolved = "";
+        res.render('quizzes/random_nomore', {
+            score
+        });
+    }else{
+        res.render('quizzes/random_result', {
+            quiz,
+            result,
+            answer,
+            score
+        });
+    }
+}
+
+renderQuiz = (req, res, next) => {
+    req.session.num = Math.floor((Math.random() * req.session.toBeResolved.length));
+    let id = req.session.toBeResolved[req.session.num];
+    const query = req;
+    const score = req.session.score;
+    const answer = query.answer || '';
+    models.quiz.findById(id)
+        .then(quiz => {
+            if (quiz) {
+                res.render('quizzes/random_play',{
+                    quiz,
+                    answer,
+                    score,
+                });
+            } else {
+                res.render('quizzes/random_noquiz',{score:0})
+                //throw new Error('There is no quizzes');
+            }
+        })
+        .catch(error => next(error));
 }
--- models/index.js
+++ models/index.js
@@ -16,58 +16,16 @@ const sequelize = new Sequelize(url);
 // Import the definition of the Quiz Table from quiz.js
 sequelize.import(path.join(__dirname, 'quiz'));
 
-// Import the definition of the Tips Table from tip.js
-sequelize.import(path.join(__dirname,'tip'));
-
-// Import the definition of the Users Table from user.js
-sequelize.import(path.join(__dirname,'user'));
-
 // Session
 sequelize.import(path.join(__dirname,'session'));
 
-//Creacion quizzes
-sequelize.sync()
-    .then(()=>
-        sequelize.models.quiz.count()
-
-    )
-    .then(count=>{
-        if(!count){
-            return sequelize.models.quiz.bulkCreate([
-                {question: "Capital de Italia", answer:"Roma"},
-                {question: "Capital de Francia", answer:"París"},
-                {question: "Capital de España", answer:"Madrid"},
-                {question: "Capital de Portugal", answer:"Lisboa"}
-            ])
-        }
-    })
-    .catch(error=>{
-        console.log(error);
-    });
-
-
 // Create tables
 sequelize.sync()
-    .then(() => console.log('Data Bases created successfully'))
-    .catch(error => {
-        console.log("Error creating the data base tables:", error);
-        process.exit(1);
-    });
+.then(() => console.log('Data Bases created successfully'))
+.catch(error => {
+    console.log("Error creating the data base tables:", error);
+    process.exit(1);
+});
 
-
-// Relation between models
-
-const {quiz, tip, user} = sequelize.models;
-
-tip.belongsTo(quiz);
-quiz.hasMany(tip);
-
-// Relation 1-to-N between User and Quiz:
-user.hasMany(quiz, {foreignKey: 'authorId'});
-quiz.belongsTo(user, {as: 'author', foreignKey: 'authorId'});
-
-// Relation 1-to-N between User and Tip:
-user.hasMany(tip,{foreignKey:'authorId'});
-tip.belongsTo(user,{as:'author',foreignKey:'authorId'});
 
 module.exports = sequelize;
